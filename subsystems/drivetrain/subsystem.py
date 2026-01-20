ac3312962318f8d87cee2e001bfe7b038275ae87
e-bauman <ebauman26@choate.edu>
2026-01-20 14:50:42 -0500

    changing things to fit our file structure

--- subsystems/drivetrain/subsystem.py
+++ subsystems/drivetrain/subsystem.py
@@ -7,7 +7,7 @@ from wpilib import DriverStation, Notifier, RobotController
 from wpilib.sysid import SysIdRoutineLog
 from wpimath.geometry import Pose2d, Rotation2d
 
-from generated.tuner_constants import TunerSwerveDrivetrain
+from subsystems.drivetrain.constants import TunerSwerveDrivetrain
 
 
 class CommandSwerveDrivetrain(Subsystem, TunerSwerveDrivetrain):
