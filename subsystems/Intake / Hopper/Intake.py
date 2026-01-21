caa6722aa73ee22c24bf8156024d54d6e2f5d447
theA-Train <146664346+theA-Train@users.noreply.github.com>
2026-01-21 15:53:37 -0500

    Co-authored-by: achung28 <achung28@users.noreply.github.com>

--- subsystems/Intake / Hopper/Intake.py
+++ subsystems/Intake / Hopper/Intake.py
@@ -17,45 +17,62 @@ class Intake(commands2.Subsystem):
         self.horizontal_motor_out = controls.DutyCycleOut(0)
        
         self.horizontal_motor_configs = (
-            constants.TalonFXConfiguration()
+            configs.TalonFXConfiguration()
             .with_motor_output(
-                constants.MotorOutputConfigs()
+                configs.MotorOutputConfigs()
                 .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
                 .with_neutral_mode(signals.NeutralModeValue.BRAKE)
+                
             )
         )
 
         self.pivot_motor_configs = (
-            constants.TalonFXConfiguration()
+            configs.TalonFXConfiguration()
             .with_motor_output(
-                constants.MotorOutputConfigs()
+                configs.MotorOutputConfigs()
                 .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
                 .with_neutral_mode(signals.NeutralModeValue.BRAKE)
             ).with_feedback(
-                constants.FeedbackConfigs()
-                .with_feedback_remote_sensor_id(signals.FeedbackSensorSourceValue.FUSED_CANCODER)
+                configs.FeedbackConfigs()
+                .with_feedback_remote_sensor_id(signals.FeedbackSensorSourceValue.FUSED_CANCODER.value) 
+                .with_sensor_to_mechanism_ratio(5) # placeholder
             ).with_motion_magic(
-                constants.MotionMagicConfigs()
-                .with_motion_magic_cruise_velocity(97)
+                configs.MotionMagicConfigs()
+                # .with_motion_magic_cruise_velocity(0) placeholder
             )
         )
+        self.pivot_motor_zeroed = False
 
     def init(self):
         self.horizontal_motor.configurator.apply(self.horizontal_motor_configs)
-        self.horizontal_motor.init()
         self.pivot_motor.configurator.apply(self.pivot_motor_configs)
-        self.pivot_motor.init()
+
+    def intake_fuel(self):
+        self.horizontal_motor.set_control(controls.DutyCycleOut.with_output(constants.intake_fuel_speed))
+
+    def get_pivot_motor_current(self):
+        return self.pivot_motor.get_supply_current().value
+    
+    def zero_pivot(self):
         self.pivot_angle = (
             (self.encoder.get_absolute_position().value - constants.intake_encoder_zero) / (2 * math.pi))
+        pos = self.pivot_angle
+        self.pivot_motor.set_position(pos)
+        self.pivot_motor_zeroed = True
 
     def get_pivot_angle(self):
-        return self.pivot_angle.getposition().value
-        
+        return (self.pivot_motor.get_position().value /
+                (2
+                * math.pi))
+       
     def stop_pivot(self):
-        self.pivot_motor.set_control(controls.DutyCycleOut.with_output(self,0))
+        self.pivot_motor.set_control(controls.DutyCycleOut.with_output(0.0))
         
-    def is_at_angle(self, angle: float):
-        return abs(self.get_pivot_angle()- angle) < constants.intake_angle_threshold)
-        
-    def 
-    
+    def is_at_angle(self):
+        return abs(self.get_pivot_angle() - constants.intake_drop_angle) < constants.intake_angle_threshold
+
+    def drop_intake(self):
+        self.req = controls.VoltageOut(constants.intake_drop_voltage)
+        self.pivot_motor.set_control(self.req)
+
+
--- subsystems/Intake / Hopper/constants.py
+++ subsystems/Intake / Hopper/constants.py
@@ -1,2 +1,8 @@
-horizontal_motor_id = 12213213 #placeholder
-pivot_motor_id = 421234412312342134 #placeholder
+horizontal_motor_id = 25 #placeholder
+pivot_motor_id = 24 #placeholder
+intake_cancoder_id = 26 #placeholder
+intake_angle_threshold = 2.0 
+intake_encoder_zero = 0.0 
+intake_drop_angle = 40 
+intake_drop_voltage = 0 
+intake_fuel_speed = 0.0 
