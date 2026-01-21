b207f54aea95863348749f7e87b5d563ef3bad8b
achung28 <achung28@choate.edu>
2026-01-21 16:05:17 -0500

    Fixed most issues

--- subsystems/climber/constants.py
+++ subsystems/climber/constants.py
@@ -1,7 +1,9 @@
 from phoenix6.hardware import CANcoder
-from phoenix6 import StatusSignal, controls, configs, hardware, signals 
-leader_motor_id = 123123231312123 #placeholder
-follower_motor_id = 3132132123312 # placeholder
+from phoenix6 import StatusSignal, controls, configs, hardware, signals
+leader_motor_id = 26 #placeholder
+follower_motor_id = 27 # placeholder
+climber_motion_magic_cruise_velocity = 97 #placeholder
+climber_gear_ratio = 2
 
 leader_motor_configs = (
             configs.TalonFXConfiguration()
@@ -9,5 +11,45 @@ leader_motor_configs = (
                 configs.MotorOutputConfigs()
                 .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
                 .with_neutral_mode(signals.NeutralModeValue.BRAKE)
+            ).with_motion_magic(
+                configs.MotionMagicConfigs()
+                .with_motion_magic_cruise_velocity(climber_motion_magic_cruise_velocity)
+                
+            ).with_slot0(
+                configs.Slot0Configs() #all placeholders
+                .with_k_p(2)
+                .with_k_i(0)
+                .with_k_d(0)
+                .with_k_s(0.195)
+                .with_k_v(0)
+                .with_k_a(0)
             )
-        )
+)
+
+
+follower_motor_configs = (
+            configs.TalonFXConfiguration()
+            .with_motor_output(
+                configs.MotorOutputConfigs()
+                .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
+                .with_neutral_mode(signals.NeutralModeValue.BRAKE)
+
+            ).with_feedback(
+                configs.FeedbackConfigs()
+                .with_feedback_remote_sensor_id(signals.FeedbackSensorSourceValue.FUSED_CANCODER.value)
+                .with_sensor_to_mechanism_ratio(climber_gear_ratio)
+                
+            ).with_motion_magic(
+                configs.MotionMagicConfigs()
+                .with_motion_magic_cruise_velocity(climber_motion_magic_cruise_velocity) 
+                
+            ).with_slot0(
+                configs.Slot0Configs() #all placeholders
+                .with_k_p(2)
+                .with_k_i(0)
+                .with_k_d(0)
+                .with_k_s(0.195)
+                .with_k_v(0)
+                .with_k_a(0)
+            )
+)
