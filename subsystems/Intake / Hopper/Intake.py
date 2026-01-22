9596e8337d2a64bf530e22b7418639786cd95419
theA-Train <146664346+theA-Train@users.noreply.github.com>
2026-01-22 09:58:42 -0500

    move to constants and cached

--- subsystems/Intake / Hopper/Intake.py
+++ subsystems/Intake / Hopper/Intake.py
@@ -11,68 +11,43 @@ class Intake(commands2.Subsystem):
         super().__init__()
         self.encoder: CANcoder = CANcoder(constants.intake_cancoder_id, '5')
         self.horizontal_motor = hardware.TalonFX(constants.horizontal_motor_id)
-        
-                
+
         self.pivot_motor = hardware.TalonFX(constants.pivot_motor_id)
         self.horizontal_motor_out = controls.DutyCycleOut(0)
-       
-        self.horizontal_motor_configs = (
-            configs.TalonFXConfiguration()
-            .with_motor_output(
-                configs.MotorOutputConfigs()
-                .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
-                .with_neutral_mode(signals.NeutralModeValue.BRAKE)
-                
-            )
-        )
-
-        self.pivot_motor_configs = (
-            configs.TalonFXConfiguration()
-            .with_motor_output(
-                configs.MotorOutputConfigs()
-                .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
-                .with_neutral_mode(signals.NeutralModeValue.BRAKE)
-            ).with_feedback(
-                configs.FeedbackConfigs()
-                .with_feedback_remote_sensor_id(signals.FeedbackSensorSourceValue.FUSED_CANCODER.value) 
-                .with_sensor_to_mechanism_ratio(5) # placeholder
-            ).with_motion_magic(
-                configs.MotionMagicConfigs()
-                # .with_motion_magic_cruise_velocity(0) placeholder
-            )
-        )
+        self.pivot_request = controls.DutyCycleOut(0.0)
+    
         self.pivot_motor_zeroed = False
 
     def init(self):
-        self.horizontal_motor.configurator.apply(self.horizontal_motor_configs)
-        self.pivot_motor.configurator.apply(self.pivot_motor_configs)
+        self.horizontal_motor.configurator.apply(constants.horizontal_motor_configs)
+        self.pivot_motor.configurator.apply(constants.pivot_motor_configs)
 
     def intake_fuel(self):
-        self.horizontal_motor.set_control(controls.DutyCycleOut.with_output(constants.intake_fuel_speed))
+        self.horizontal_motor.set_control(self.horizontal_motor_out.with_output(constants.intake_fuel_speed))
 
     def get_pivot_motor_current(self):
-        return self.pivot_motor.get_supply_current().value
+        return self.pivot_motor.get_supply_current()
+    
+    def get_horizontal_motor_current(self):
+        return self.horizontal_motor.get_supply_current()
     
     def zero_pivot(self):
         self.pivot_angle = (
-            (self.encoder.get_absolute_position().value - constants.intake_encoder_zero) / (2 * math.pi))
+            (self.encoder.get_absolute_position().value))
         pos = self.pivot_angle
         self.pivot_motor.set_position(pos)
         self.pivot_motor_zeroed = True
 
     def get_pivot_angle(self):
-        return (self.pivot_motor.get_position().value /
-                (2
-                * math.pi))
+        return (self.pivot_motor.get_position().value)
        
     def stop_pivot(self):
-        self.pivot_motor.set_control(controls.DutyCycleOut.with_output(0.0))
+        self.pivot_motor.set_control(self.pivot_request.with_output(0.0))
         
-    def is_at_angle(self):
-        return abs(self.get_pivot_angle() - constants.intake_drop_angle) < constants.intake_angle_threshold
+    def is_at_angle(self, angle: float):
+        return abs(self.get_pivot_angle() - angle) < constants.intake_angle_threshold
 
-    def drop_intake(self):
-        self.req = controls.VoltageOut(constants.intake_drop_voltage)
-        self.pivot_motor.set_control(self.req)
+    def deploy_intake(self, angle:float):
+        self.pivot_motor.set_position(angle)
 
 
--- subsystems/Intake / Hopper/constants.py
+++ subsystems/Intake / Hopper/constants.py
@@ -1,3 +1,6 @@
+from unittest import signals
+from phoenix6 import StatusSignal, controls, configs, hardware, signals 
+
 horizontal_motor_id = 25 #placeholder
 pivot_motor_id = 24 #placeholder
 intake_cancoder_id = 26 #placeholder
@@ -6,3 +9,38 @@ intake_encoder_zero = 0.0
 intake_drop_angle = 40 
 intake_drop_voltage = 0 
 intake_fuel_speed = 0.0 
+
+horizontal_motor_configs = (
+    configs.TalonFXConfiguration()
+    .with_motor_output(
+        configs.MotorOutputConfigs()
+        .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
+        .with_neutral_mode(signals.NeutralModeValue.BRAKE)
+                
+        )
+)
+
+pivot_motor_configs = (
+            configs.TalonFXConfiguration()
+            .with_motor_output(
+                configs.MotorOutputConfigs()
+                .with_inverted(signals.InvertedValue.CLOCKWISE_POSITIVE)
+                .with_neutral_mode(signals.NeutralModeValue.BRAKE)
+            ).with_feedback(
+                configs.FeedbackConfigs()
+                .with_feedback_remote_sensor_id(signals.FeedbackSensorSourceValue.FUSED_CANCODER.value) 
+                .with_sensor_to_mechanism_ratio(5) # placeholder
+            ).with_motion_magic(
+                configs.MotionMagicConfigs()
+                # .with_motion_magic_cruise_velocity(0) placeholder
+            ).with_slot0(
+                configs.Slot0Configs()
+                .with_k_p(0.0)
+                .with_k_i(0.0)
+                .with_k_d(0.0)
+                .with_k_s(0.0)
+                .with_k_v(0)
+                .with_k_a(0)
+                .with_gravity_type(signals.GravityTypeValue.ARM_COSINE)
+            )
+        )
