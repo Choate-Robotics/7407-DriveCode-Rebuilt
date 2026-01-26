135112ed9eb58d6b85254628fae24c260b4055d0
alexl1239 <aluo27@choate.edu>
2026-01-25 19:02:56 -0500

    updated autoroutine

--- autos/auto_routine.py
+++ autos/auto_routine.py
@@ -2,7 +2,7 @@ from commands2 import Command
 from wpimath.geometry import Pose2d
 
 import math
-from utils.field_constants import FieldConstants
+from utils.field_constants import LinesVertical, LinesHorizontal
 from wpimath.geometry import Rotation2d, Translation2d
 
 class AutoRoutine:
@@ -10,5 +10,5 @@ class AutoRoutine:
         self.command = command
         self.blue_start_pose = start_pose
         self.red_start_pose = start_pose.rotateAround(
-            Translation2d(FieldConstants.field_length/2, FieldConstants.field_width/2),
+            Translation2d(LinesVertical.CENTER, LinesHorizontal.CENTER),
             Rotation2d(math.pi/2))
