b7c7f84cee8d8abac7f12727dde310595d571905
alexl1239 <aluo27@choate.edu>
2026-01-30 16:54:40 -0500

    got rid of prints

--- autos/leave.py
+++ autos/leave.py
@@ -13,8 +13,6 @@ paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(1)]
 def auto(robot_container: RobotContainer) -> AutoRoutine:
     command = SequentialCommandGroup(
         AutoBuilder.followPath(paths[0]),
-        print(f"\n{'='*20}\nDEBUG: {"auto complete:"}\n{'='*20}\n"),
         robot_container.drivetrain.apply_request(lambda: requests.Idle()),
-        print(f"\n{'='*20}\nDEBUG: {"idling"}\n{'='*20}\n"),
     )
     return AutoRoutine(command, paths[0].getStartingHolonomicPose())
