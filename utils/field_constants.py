c7fa67efd13239ea5761971a41f19241b71c601c
e-bauman <ebauman26@choate.edu>
2026-02-03 17:16:26 -0500

    alliance flip util

--- utils/field_constants.py
+++ utils/field_constants.py
@@ -32,20 +32,6 @@ APRILTAG_WIDTH = inchesToMeters(6.5)
 # Field dimensions
 FIELD_LENGTH = _layout.getFieldLength()
 FIELD_WIDTH = _layout.getFieldWidth()
-
-def get_red(blue_value: Pose2d | Translation2d | Translation3d | float):
-    if type(blue_value) == Pose2d | type(blue_value) == Translation2d | type(blue_value) == Translation3d:
-        return blue_value.rotateAround(
-            Translation2d(LinesVertical.CENTER, LinesHorizontal.CENTER),
-            Rotation2d(math.pi))
-    else:
-        return FIELD_LENGTH - blue_value
-
-def get_alliance(value: Pose2d | Translation2d | Translation3d | float) -> Pose2d:
-    if DriverStation.getAlliance() == DriverStation.Alliance.kBlue:
-        return value
-    else:
-        return get_red(value)
     
 def get_alliance_less_than(value: float, value_to_compare_to: float):
     if DriverStation.getAlliance() == DriverStation.Alliance.kBlue:
