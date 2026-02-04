d0a7d3f66928b9c6a722145cd092e5b383fc28c7
Adi <112349665+avariar27@users.noreply.github.com>
2026-02-04 15:22:48 -0500

    scrolling function is sus

--- sensors/constants.py
+++ sensors/constants.py
@@ -3,3 +3,7 @@
 #leds
 leds_id = 30 #placeholder
 led_length = 60 #placeholder
+
+rainbow_saturation = 255 #placeholder
+rainbow_value = 255 #placeholder
+scroll_speed = 0 #placeholder, in percent of whole LED strip per second
--- sensors/leds.py
+++ sensors/leds.py
@@ -1,15 +1,19 @@
-from wpilib import AddressableLED
+from wpilib import AddressableLED, LEDPattern, Color
 import ntcore
 import constants
 from commands2 import Subsystem
+from math import 
 
 class LEDs(Subsystem):
 
     def __init__(self):
 
         self.led = AddressableLED(constants.leds_id)
-        self.led 
-        #self.led_buffer = 
+        self.led.setLength(constants.led_length)
+        self.led_buffer = [self.led.LEDData() for i in range(constants.led_length)]
+
+        self.pattern = LEDPattern.solid(Color.kWhite)
+
     def enable_leds(self):
         """
         enables the LEDs
@@ -21,3 +25,52 @@ class LEDs(Subsystem):
         disables the LEDs
         """
         self.led.stop()
+
+    def update_leds(self):
+        """
+        updates LED pattern
+        """
+        self.pattern.applyTo(self.led_buffer)
+        self.led.setData(self.led_buffer)
+
+    def set_solid(self, r: int, g: int, b: int):
+        """
+        sets LEDs as a solid color
+        """
+        self.pattern = LEDPattern.solid(Color(r/255, g/255, b/255))
+        self.update_leds()
+
+    def set_rainbow(self):
+        """
+        sets LEDs to a rainbow pattern
+        """
+        self.pattern = LEDPattern.rainbow(constants.rainbow_saturation, constants.rainbow_value)
+        self.update_leds()
+
+    def set_gradient(self, r1: int, g1: int, b1: int, r2: int, g2: int, b2: int):
+        """
+        sets LEDs to a (continuous) gradient pattern
+        """
+        self.pattern = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, Color(r1/255, g1/255, b1/255), Color(r2/255, g2/255, b2/255))
+        self.update_leds()
+
+    def set_progress_mask(self, current_value, max_value, r: int, g: int, b: int):
+        """
+        sets progress bar of any color of choice pattern for LEDs 
+        """
+        self.pattern = LEDPattern.progressMaskLayer(current_value / max_value).solid(Color(r/255, g/255, b/255))
+        self.update_leds()
+    
+    def reverse_pattern(self):
+        """
+        reverses direction of given pattern
+        """
+        self.pattern.reversed()
+        self.update_leds()
+    
+    def set_scroll(self, r1: int, g1: int, b1: int, r2: int, g2: int, b2: int):
+        """
+        sets a scrolling pattern for the LEDs
+        """
+        self.set_gradient(r1, g1, b1, r2, g2, b2)
+        self.pattern.scrollAtRelativeSpeed(constants.scroll_speed) 
