0a4f4ca91b87eff1f170cf1e705341bc80cf5ed0
Adi <112349665+avariar27@users.noreply.github.com>
2026-02-12 16:57:39 -0500

    outpost and depot auto

---
+++ autos/outpost_depot.py
@@ -0,0 +1,29 @@
+from pathplannerlib.path import PathPlannerPath
+from pathplannerlib.auto import AutoBuilder
+
+from phoenix6.swerve import requests
+from autos import AutoRoutine
+from robotcontainer import RobotContainer
+import robot_constants
+from subsystems import *
+
+from commands2 import SequentialCommandGroup, ParallelCommandGroup, InstantCommand, ParallelDeadlineGroup
+
+path_name = "OutpostandDepot"
+paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(11)]
+
+def auto(robot_container: RobotContainer) -> AutoRoutine:
+    command = SequentialCommandGroup(
+        AutoBuilder.followPath(paths[0]),
+        DeployIntake(robot_container.intake),
+        AutoBuilder.followPath(paths[1]),
+        ParallelCommandGroup(
+            AimDrivetrainAuto(robot_container.drivetrain),
+            SetShooterAuto(robot_container.shooter)
+        ),
+        RunIndexer(robot_container.indexer).withTimeout(robot_constants.auto_shooting_timeout),
+        AutoBuilder.followPath(paths[2]),
+        DeployClimbL1(robot_container.climber),
+        robot_container.drivetrain.apply_request(lambda: requests.Idle())
+    )
+    return AutoRoutine(command, paths[0].getStartingHolonomicPose())
