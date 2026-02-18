77c1ea97f3a133ab68882a5315e688dc69deb9c6
achung28 <achung28@choate.edu>
2026-02-18 15:21:36 -0500

    t

--- autos/auto_routine.py
+++ autos/auto_routine.py
@@ -2,6 +2,6 @@ from commands2 import Command
 from wpimath.geometry import Pose2d
 
 class AutoRoutine:
-    def __init__(self, command: Command, start_pose: Pose2d):
+    def __init__(self, command: Command, start_pose: Pose2d | None):
         self.command = command
         self.start_pose = start_pose
--- autos/one_loop_climb.py
+++ autos/one_loop_climb.py
@@ -9,31 +9,33 @@ from commands2 import SequentialCommandGroup, ParallelCommandGroup, InstantComma
 
 
 path_name = "OneLoopClimb"
-paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(1)]
+paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(6)]
+
 def auto(robot_container: RobotContainer) -> AutoRoutine:
     command = SequentialCommandGroup(
         ParallelCommandGroup(
             AutoBuilder.followPath(paths[0]),
-            DeployIntake(robot_container.intake)
-        ),
-
-        ParallelCommandGroup(
-            AutoBuilder.followPath(paths[1]),
+            SequentialCommandGroup(
+            DeployIntake(robot_container.intake),
             RunIntake(robot_container.intake)
+            )
+        ),
+        SequentialCommandGroup(
+            AutoBuilder.followPath(paths[1]),
+            #stop intake from running?
+            AutoBuilder.followPath(paths[2]),
+
         ),
 
-        AutoBuilder.followPath(paths[2]),
+        SequentialCommandGroup(
+            SetShooterAuto(robot_container.shooter, robot_container.drivetrain), 
+            RunIndexer(robot_container.indexer).withTimeout(robot_constants.auto_shooting_timeout),
+        ),
         ParallelCommandGroup(
         AutoBuilder.followPath(paths[3]),
-        SetPivot(robot_container.intake, 90.0)
+        DeployClimbL1(robot_container.climber),
         ),
-
-        ParallelCommandGroup(
-            AutoBuilder.followPath(paths[4]),
-            SetShooterAuto(robot_container.shooter, robot_container.drivetrain)
-        ),
-        RunIndexer(robot_container.indexer).withTimeout(robot_constants.auto_shooting_timeout),
-        AutoBuilder.followPath(paths[5]),
-        DeployClimbL1(robot_container.climber)
+        
+        Retract(robot_container.climber)
     )
     return AutoRoutine(command, paths[0].getStartingHolonomicPose())
