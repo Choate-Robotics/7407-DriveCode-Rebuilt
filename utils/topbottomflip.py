f494c99db5c18d44a2de188d901d5ac3aaaaea03
theA-Train <146664346+theA-Train@users.noreply.github.com>
2026-02-26 11:51:19 -0500

    top bottom auto trench flipper done
    i need to manually fix my top and bottom autos tho bc the code breaks with pose waypoints that use degrees instead of rad

--- utils/topbottomflip.py
+++ utils/topbottomflip.py
@@ -3,22 +3,35 @@ from pathlib import Path
 
 FIELD_HEIGHT = 8
 
-def waypoint_flip(output_path: Path):
-    with open("deploy/choreo/TwoLoopTopClimb.traj", "r") as f:
+# flips a trench auto to the opposite trench
+# change the "with open" line to the auto you want to flip
+# does not work with pose waypoints that use degrees for now
+# change choreo_dir to the name you want
+# run in terminal with p
+
+def waypoint_flip(output_path: Path): 
+    with open("deploy/choreo/TwoLoopTopClimb.traj", "r") as f: 
         data = json.load(f)
         waypoints = data
 
+    data["snapshot"]["waypoints"] = [] # i dont even know what snapshot does in a traj file tbh
+    data["trajectory"]["samples"] = [] # emptying out the path generated from the previous waypoints
     
-    for waypoint in data["waypoints"]:
-        waypoint["y"] = FIELD_HEIGHT - waypoint["y"] # flips waypoint to bottom of field
+    for waypoint in data["params"]["waypoints"]:
+        waypoint["y"]["val"] = FIELD_HEIGHT - float(waypoint["y"]["val"]) # flips waypoint to bottom of field
+        waypoint["y"]["exp"] = str(waypoint["y"]["val"]) + " m" # flips waypoint to bottom of field
         
-        waypoint["heading"] *= -1 #flips waypoint pose / rotation
+        waypoint["heading"]["val"] *= -1 #flips waypoint pose / rotation
+        waypoint["heading"]["exp"] = str(waypoint["heading"]["val"]) + " rads" #flips waypoint pose / rotation
+
+    data["trajectory"]["samples"] = []
+        
 
     with open(output_path, "w") as f:
         json.dump(data, f, indent=2)
 
 if __name__ == "__main__":
-    choreo_dir = Path("src/main/deploy/choreo")
+    choreo_dir = Path("deploy/choreo")
 
     waypoint_flip(
         choreo_dir / "TwoLoopBottomClimb.traj"
