9797f570dda5e6c4138b939665192d6b2dfb271b
theA-Train <146664346+theA-Train@users.noreply.github.com>
2026-02-26 15:51:06 -0500

    oh my god i fixed my flip logic again

--- utils/topbottomflip.py
+++ utils/topbottomflip.py
@@ -1,4 +1,5 @@
 import json
+import math
 from pathlib import Path
 
 FIELD_HEIGHT = 8
@@ -7,10 +8,10 @@ FIELD_HEIGHT = 8
 # change the "with open" line to the auto you want to flip
 # does not work with pose waypoints that use degrees for now
 # change choreo_dir to the name you want
-# run in terminal with p
+# run in terminal 
 
 def waypoint_flip(output_path: Path): 
-    with open("deploy/choreo/TwoLoopTopClimb.traj", "r") as f: 
+    with open("deploy/choreo/testing.traj", "r") as f: 
         data = json.load(f)
         waypoints = data
 
@@ -18,11 +19,14 @@ def waypoint_flip(output_path: Path):
     data["trajectory"]["samples"] = [] # emptying out the path generated from the previous waypoints
     
     for waypoint in data["params"]["waypoints"]:
-        waypoint["y"]["val"] = FIELD_HEIGHT - float(waypoint["y"]["val"]) # flips waypoint to bottom of field
-        waypoint["y"]["exp"] = str(waypoint["y"]["val"]) + " m" # flips waypoint to bottom of field
-        
+        waypoint["y"]["val"] = FIELD_HEIGHT - float(waypoint["y"]["val"]) # flips waypoint to other side of alliance side
+        waypoint["y"]["exp"] = str(waypoint["y"]["val"]) + " m" # add "m" to signify meters for choreo
         waypoint["heading"]["val"] *= -1 #flips waypoint pose / rotation
-        waypoint["heading"]["exp"] = str(waypoint["heading"]["val"]) + " rads" #flips waypoint pose / rotation
+
+        if "rad" in waypoint["heading"]["exp"]:
+            waypoint["heading"]["exp"] = str(waypoint["heading"]["val"]) + " rad" # add "rad" to signify radians for choreo
+        else:
+            waypoint["heading"]["exp"] = str(waypoint["heading"]["val"] * (180/math.pi)) + " deg" # if your waypoints are in degrees
 
     data["trajectory"]["samples"] = []
         
@@ -34,5 +38,5 @@ if __name__ == "__main__":
     choreo_dir = Path("deploy/choreo")
 
     waypoint_flip(
-        choreo_dir / "TwoLoopBottomClimb.traj"
+        choreo_dir / "testbottom.traj"
     )
