06470aecb46b1a76fb94f63f97824ea46b928e3c
achung28 <achung28@choate.edu>
2026-03-07 10:33:52 -0500

    Create preload_climb.py

---
+++ autos/preload_climb.py
@@ -0,0 +1,27 @@
+from pathplannerlib.path import PathPlannerPath
+from pathplannerlib.auto import AutoBuilder
+from phoenix6.swerve import requests
+from autos import AutoRoutine
+from robotcontainer import RobotContainer
+import robot_constants
+from subsystems import *
+from commands2 import SequentialCommandGroup, ParallelCommandGroup, InstantCommand, ParallelDeadlineGroup
+
+
+
+path_name = "OneLoopTopClimb" or "OneLoopBottomClimb"
+paths = [PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(1)]
+def auto(robot_container: RobotContainer) -> AutoRoutine:
+    command = SequentialCommandGroup(
+        ParallelCommandGroup(
+            SetShooterAuto(robot_container.shooter, robot_container.drivetrain.get_pose()),
+            Index(robot_container.indexer, robot_container.intake).withTimeout(5),
+        ),
+        ParallelCommandGroup(
+            AutoBuilder.followPath(paths[0]),
+            DeployClimbL1(robot_container.climber)
+        ),
+
+        RetractClimb(robot_container.climber)
+    )  
+    return AutoRoutine(command, paths[0].getStartingHolonomicPose()) 
